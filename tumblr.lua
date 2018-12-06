18202b87e858bb9d74e939b82c25df1de80538ac
kiska <qingbest@fastmail.fm>
2018-12-07 05:21:31 +1100

    Updated to 20181207.04; Skipping 403 errors on assets.tumblr.com

--- tumblr.lua
+++ tumblr.lua
@@ -151,8 +151,8 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
   end
   
   if status_code >= 500 or
-    (status_code >= 400 and status_code ~= 404) or
-    status_code == 0 then
+    (status_code > 400 and status_code < 403 status_code ~= 404) or
+    status_code > 404 then
     io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
     io.stdout:flush()
     os.execute("sleep 1")
@@ -170,7 +170,33 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
       return wget.actions.CONTINUE
     end
   end
-
+  
+  if status_code == 403 or status_code == 400 or status_code == 0 then
+    --if string.match(url["host"], "")
+    if string.match(url["host"], "assets%.tumblr%.com") then
+      io.stdout:write("Server returned " ..http_stat.statcode.." ("..err.."). Skipping.\n")
+      tries = 0
+      return wget.actions.EXIT
+    else
+      io.stdout:write("Server returned " ..http_stat.statcode.." ("..err.."). Sleeping.\n")
+      io.stdout:flush()
+      os.execute("sleep 1")
+      tries = tries + 1
+      if tries >= 5 then
+        io.stdout:write("\nI give up...\n")
+        io.stdout:flush()
+        tries = 0
+        if allowed(url["url"], nil) then
+          return wget.actions.ABORT
+        else
+          return wget.actions.EXIT
+        end
+      else
+        return wget.actions.CONTINUE
+      end
+    end
+  end
+  
   tries = 0
 
   local sleep_time = 0
