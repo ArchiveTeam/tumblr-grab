eefaa57733e2b1108e28908fa809e41873c50fca
marked <marked@users.noreply.github.com>
2018-12-13 05:56:30 -0600

    Better block for /embed

--- tumblr.lua
+++ tumblr.lua
@@ -150,6 +150,7 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
   end
 
   if string.match(url, "^https?://www.tumblr.com/oembed/1.0")
+  or string.match(url, "^https?://" .. item_value .. "%.tumblr%.com/.*/embed$")
   or string.match(url, "^https?://[0-9]+%.media%.tumblr%.com/avatar_[a-zA-Z0-9]+_64%.pnj")
   or string.match(url, "^https?://[0-9]+%.media%.tumblr%.com/avatar_[a-zA-Z0-9]+_64%.gif")
   or string.match(url, "^https?://[0-9]+%.media%.tumblr%.com/avatar_[a-zA-Z0-9]+_16%.pnj")
