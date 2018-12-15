075eabdf06550055213735d6cebc5750e556ff6c
kiska3 <qingbest@fastmail.fm>
2018-12-15 18:16:27 +1100

    Change sleep time between errors

--- tumblr.lua
+++ tumblr.lua
@@ -251,7 +251,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     or status_code > 404 then
     io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
     io.stdout:flush()
-    os.execute("sleep 10")
+    os.execute("sleep 60")
     tries = tries + 1
     if tries >= 5 then
       io.stdout:write("\nI give up...\n")
